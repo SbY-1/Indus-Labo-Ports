c105dfb167c7168ca374c4f3cc798ada7c452010
Sebastien <schmidt.sebastien@gmail.com>
2015-02-02 11:48:50 +0100

    Ajout de nouveaux fichiers

---
+++ Gestion.h
@@ -0,0 +1,14 @@
+#ifndef GESTION_H
+#define GESTION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROP_FILE	"config.cfg"
+
+char* getProp(const char *fileName, const char *propName);
+
+#endif /* GESTION_H */
